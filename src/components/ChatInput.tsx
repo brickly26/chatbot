ab73cde784f640443a30279f1df2175cdc068b75
brickly26 <sabburak26@gmail.com>
2023-05-02 09:44:49 -0400

    set up react query usemutation hook to send a message

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { nanoid } from "nanoid";
 import { FC, HTMLAttributes, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
@@ -9,11 +11,40 @@ interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
   const [input, setInput] = useState<string>("");
 
+  const { mutate: sendMessage, isLoading } = useMutation({
+    mutationFn: async (message: Message) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages: "hello" }),
+      });
+
+      return response.body;
+    },
+    onSuccess: () => {
+      console.log("success");
+    },
+  });
+
   return (
     <div {...props} className={cn("border-t border-zinc-300", className)}>
       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
         <TextareaAutosize
           rows={2}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              const message = {
+                id: nanoid(),
+                isUserInput: true,
+                text: input,
+              };
+
+              sendMessage(message);
+            }
+          }}
           maxRows={4}
           value={input}
           onChange={(e) => setInput(e.target.value)}
